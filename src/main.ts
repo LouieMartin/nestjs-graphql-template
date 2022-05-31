9499f97c20e5a63e17e0061823eb97fb6a3d484a
LouieMartin <louie.decierdo@gmail.com>
2022-05-31 12:53:11 +0800

    Fixed a vulnerability

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import type { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+(async () => {
+  const adapter = new FastifyAdapter();
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    adapter,
+  );
+
+  app.setGlobalPrefix('api');
+
   await app.listen(3000);
-}
-bootstrap();
+})();
